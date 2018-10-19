20cfa0d52beda73cb5f588443d3934dd8ce118e2
Moutaman Kamal <mkgodot96@gmail.com>
2018-10-19 20:22:47 +0200

    Used request module to get the access token from github,but it's not working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const request = require('request')
 const fs = require('fs')
 const port = 3000
 const config = JSON.parse(fs.readFileSync('config.json'))
@@ -12,16 +13,26 @@ app.get('/login/github', (req, res) => {
 	console.log('Redirectig')
 	// 1. Users are redirected to request their GitHub identity
 	res.redirect(
-		`https://github.com/login/oauth/authorize?
-		client_id=${config.client_id}
-		&scope=${config.scope}`
+		`https://github.com/login/oauth/authorize?client_id=${
+			config.client_id
+		}&scope=${config.scope}`
 	)
 	console.log('Redirected')
 })
 // 2. Users are redirected back from GitHub with a code used to get an access token
 app.get('/authenticated', (req, res) => {
 	code = req.query.code
-	res.send(`Code: ${code}`)
+	options = {
+		url: `https://github.com/login/oauth/access_token?client_id=${
+			config.client_id
+		}&client_secret=${config.client_secret}&code=${code}`,
+		method: 'post',
+		headers: { Accept: 'application/json' }
+	}
+	// res.send(`Code: ${code}`)
+	request(options).on('response', function(response) {
+		res.send(JSON.stringify(response))
+	})
 })
 
 app.listen(port, () => {
